87991120cd89285e5f40a6c2a93fde3e682eef8d
Duncan <semunda9@gmail.com>
2020-04-08 19:15:52 +0200

    added rest api

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
- let express=require("express");
+const http=require("http");
+let express=require("express");
 let app=express();
 let bodyParser=require('body-parser');
 let msyql=require("mysql");
@@ -24,6 +25,7 @@ connection.connect(function(error){
 );
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());// to support json file
 app.use(express.static(__dirname + '/public'));
 
 app.set("view engine","ejs");
@@ -32,13 +34,33 @@ app.get("/",function(req,res){
 res.send("HomePage");
 });
 
+//get the data in database
+app.get("/index",function(req,res){
+    connection.query('select * from trycrud',(error,rows)=>{
+        if(!error)
+        res.end(JSON.stringify(rows));
+        else
+        console.log(error);
+
+    })
+});
+
+//rest api to get a single client 
+app.get("/index/:id",(req,res)=>{
+connection.query("select * from trycrud where id=?",[req.params.id],(results,error)=>{
+    if(!error) 
+    res.end(JSON.stringify(results));
+    else
+    res.send(error);
+})
+})
 
 app.get("/SignUp",function(req,res){
     res.render("SignUP");
 })
 
-
-app.post("/home",function(req,res){
+// insert new records using restful api
+app.post("/index",function(req,res){
    
      const data={
         id:req.body.id,
@@ -46,46 +68,41 @@ app.post("/home",function(req,res){
    }
  
    
-    connection.query('insert into trycrud SET ?',data,function(error){
+    connection.query('insert into trycrud SET ?',data,function(error,results){
         if(error)
         console.log(error);
         else
-        console.log("data saved")
+        res.end(JSON.stringify(results));
     });
 
 res.redirect("index");
 
 });
-
-app.post("/delete/:id",function(req,res){
-    connection.query('delete from trycrud where id =?',[req.params.id],(error,results)=>{
-res.redirect("/index");
+// rest api for delete
+app.delete("/index",function(req,res){
+    connection.query('delete from trycrud where id =?',[req.body.id],(error,results)=>{
+        if(error)
+        res.send(error)
+res.end('Record deleted');
     })
 })
 
-app.get("/edit/:id",function(req,res){
-    connection.query('select *  from trycrud where id=?',[req.params.id],(error,results)=>{
-        res.render('new',{item:results[0]});
-    });
-});
+// rest api for update
 
-app.post("/update/:id",function(req,res){
-    connection.query('update trycrud set firstName  = ? where id=?',[req.body.itemName,req.params.id],(error,results)=>{
-        res.redirect("/index");
-    });
-});
-
-//get the data in database
-app.get("/index",function(req,res){
-    connection.query('select * from trycrud',(error,rows)=>{
-        if(!error)
-        res.render("index",{items:rows});
+app.put("/index",function(req,res){
+    connection.query('update trycrud set firstName  = ? where id=?',[req.body.firstName,req.body.id],(error,results)=>{
+        if(error)
+        res.send(error);
         else
-        console.log(error);
-
-    })
+        res.end(JSON.stringify(results))
+    });
 });
 
-app.listen(3001,function(){
-    console.log("server up");
+
+// Port Listening
+
+let server=app.listen(3001,"127.0.0.1",function(){
+    let host=server.address().address
+    let port=server.address().port
+    console.log("server up",host,port);
 });
